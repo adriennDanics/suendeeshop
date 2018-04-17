fe8cafbbb39ae4fdc995b0c8a83ce9ba073e8655
Ágnes Gál <galagnes277@gmail.com>
2018-04-17 15:18:45 +0200

    Item page update with html site

--- src/main/java/com/codecool/shop/controller/Checkout.java
+++ src/main/java/com/codecool/shop/controller/Checkout.java
@@ -1,4 +1,41 @@
 package com.codecool.shop.controller;
 
-public class Checkout {
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.ShoppingCart;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.ShoppingCartMem;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/item"})
+public class Checkout extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        ShoppingCart shoppingCart = ShoppingCartMem.getInstance();
+
+//        Map params = new HashMap<>();
+//        params.put("category", productCategoryDataStore.find(1));
+//        params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+//        context.setVariables(params);
+        context.setVariable("recipient", "World");
+        context.setVariable("category", productCategoryDataStore.find(1));
+        context.setVariable("products", ((ShoppingCartMem) shoppingCart).getAll());
+        engine.process("product/item.html", context, resp.getWriter());
+    }
 }
