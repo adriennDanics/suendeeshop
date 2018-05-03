dc027e59a77c6ee2e5f3d78a268f62478456e614
Adrienn <adrienn.d91@gmail.com>
2018-05-03 14:47:40 +0200

    DAOJDBC files implemented

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBC.java
@@ -3,9 +3,11 @@ package com.codecool.shop.dao.implementation;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.config.loadConfigJDBC;
+import com.sun.org.apache.regexp.internal.RE;
 
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -15,6 +17,15 @@ public class ProductCategoryDaoJDBC implements ProductCategoryDao {
     private static final String DATABASE = "jdbc:postgresql://" + properties.getProperty("url") + "/" + properties.getProperty("database");
     private static final String USER = properties.getProperty("user");
     private static final String PASSWORD = properties.getProperty("password");
+    private static ProductCategoryDaoJDBC instance = null;
+
+
+    public static ProductCategoryDaoJDBC getInstance() {
+        if (instance == null) {
+            instance = new ProductCategoryDaoJDBC();
+        }
+        return instance;
+    }
 
     @Override
     public void add(ProductCategory category) {
@@ -38,14 +49,46 @@ public class ProductCategoryDaoJDBC implements ProductCategoryDao {
 
     @Override
     public ProductCategory find(int id) {
-        return null;
-        //TODO
+
+        String name = null;
+        String department = null;
+        String description = null;
+
+        try(Connection con = getConnection()) {
+            String query = "SELECT * FROM productcategories WHERE id = ?;";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet results = statement.executeQuery();
+
+            while (results.next()){
+                name = results.getString("name");
+                department = results.getString("department");
+                description = results.getString("description");
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return new ProductCategory(name, department, description);
     }
 
     @Override
     public List<ProductCategory> getAll() {
-        return null;
-        //TODO
+        List<ProductCategory> productCategories = new ArrayList<>();
+
+        try(Connection con = getConnection()) {
+            String query = "SELECT id FROM productcategories WHERE name IS NOT NULL ;";
+            PreparedStatement statement = con.prepareStatement(query);
+            ResultSet results = statement.executeQuery();
+
+            while (results.next()){
+                productCategories.add(this.find(results.getInt("id")));
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return productCategories;
     }
 
     private Connection getConnection() throws SQLException {
--- src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoJDBC.java
@@ -1,36 +1,97 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.config.loadConfigJDBC;
+import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ShoppingCartDao;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.sun.javafx.collections.MappingChange;
 
 import java.sql.*;
-import java.util.Map;
+import java.util.*;
 
 public class ShoppingCartDaoJDBC implements ShoppingCartDao {
+    private static Properties properties = loadConfigJDBC.getProperties();
+    private static final String DATABASE = "jdbc:postgresql://" + properties.getProperty("url") + "/" + properties.getProperty("database");
+    private static final String USER = properties.getProperty("user");
+    private static final String PASSWORD = properties.getProperty("password");
+    private Map<Product, Integer> shoppingCartMap = new HashMap<>();
+
+
     @Override
     public void add(Product product) {
-        //TODO
+        try(Connection con = getConnection()) {
+            String query = "INSERT INTO shoppingcart(user_id, product_id) VALUES (1,?);";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1, product.getId());
+            statement.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public void remove(int id) {
-        //TODO
+        try(Connection con = getConnection()) {
+            String query = "DELETE FROM shoppingcart WHERE id = (SELECT id FROM shoppingcart WHERE product_id = ? LIMIT 1);";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1, id);
+            statement.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public Map<Product, Integer> getAll() {
-        return null;
-        //TODO
+        ProductDao productDao = ProductDaoJDBC.getInstance();
+
+        try(Connection con = getConnection()) {
+            String query = "SELECT product_id FROM shoppingcart WHERE 'user' = 'user';";
+            PreparedStatement statement = con.prepareStatement(query);
+            ResultSet results = statement.executeQuery();
+
+            while (results.next()){
+                Product currentProduct = productDao.find(results.getInt("product_id"));
+                if (shoppingCartMap.containsKey(currentProduct)) {
+                    Integer value = shoppingCartMap.get(currentProduct);
+                    shoppingCartMap.put(currentProduct, ++value);
+                } else {
+                    shoppingCartMap.put(currentProduct, 1);
+                }
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return shoppingCartMap;
     }
 
     @Override
     public int length() {
-        return 0;
-        //TODO
+        return shoppingCartMap.size();
     }
 
     @Override
     public void clear() {
-        //TODO
+        shoppingCartMap.clear();
+        try(Connection con = getConnection()) {
+            String query = "DELETE FROM shoppingcart WHERE 'user' = 'user';";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                DATABASE,
+                USER,
+                PASSWORD
+        );
     }
 }
